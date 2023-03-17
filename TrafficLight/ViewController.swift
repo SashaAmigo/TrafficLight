ce66111be77a45416c3e8b2255355a36d80eee45
ALEXANDR SINYUTIN <shura.sinyutin@icloud.com>
2023-03-17 19:55:38 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Саша Amigo on 17.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
