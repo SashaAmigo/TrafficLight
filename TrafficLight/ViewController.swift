854482b5c8d149fad1deacd21b84ceb4933a6ad3
ALEXANDR SINYUTIN <shura.sinyutin@icloud.com>
2023-03-18 22:30:44 +0300

    Rounded view and button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+        greenLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        redLightView.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        greenLightView.alpha = 1
+        sender.setTitle("CONTINUE", for: .normal)
+        
+    }
+    
 }
-
+	
